36c32ae066b8e86ce1a1899cd471e85dccd13a4f
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2023-11-06 16:25:59 +0300

    Added sending geolocation

--- task5/script.js
+++ task5/script.js
@@ -1,6 +1,7 @@
 let message = document.querySelector("#message");
 let btnSendMessage = document.querySelector("#send_message");
 let boxMessages = document.querySelector(".box-messages");
+let btnSendGeo = document.querySelector("#send_geo");
 
 btnSendMessage.addEventListener("click", function (e) {
     e.preventDefault();
@@ -12,6 +13,25 @@ btnSendMessage.addEventListener("click", function (e) {
         ws.send(textMessage);
     };
 });
+
+btnSendGeo.addEventListener("click", function (e) {
+    e.preventDefault();
+    displayGeo();
+});
+
+// typeSender = "client" or "server"
+function addMessageToBox(message, typeSender) {
+    let messageElement = document.createElement("div");
+    messageElement.classList.add("message", `message--${typeSender}`);
+
+    if (typeof message == "string") {
+        messageElement.innerText = message;
+        boxMessages.appendChild(messageElement);
+    } else if (typeof message == "object") {
+        messageElement.appendChild(message);
+        boxMessages.appendChild(messageElement);
+    }
+}
 
 function createWS() {
     const websocket = new WebSocket("wss://echo-ws-service.herokuapp.com/");
@@ -30,12 +50,29 @@ function createWS() {
     return websocket;
 }
 
-// typeSender = "client" or "server"
-function addMessageToBox(message, typeSender) {
-    let messageElement = document.createElement("div");
+function displayGeo() {
+    function success(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        let geoLink = document.createElement("a");
 
-    messageElement.innerText = message;
-    messageElement.classList.add("message", `message--${typeSender}`);
+        geoLink.setAttribute(
+            "href",
+            `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`
+        );
+        geoLink.setAttribute("target", "_blank");
+        geoLink.innerText = "Гео-локация";
 
-    boxMessages.appendChild(messageElement);
+        addMessageToBox(geoLink, "client");
+    }
+
+    function error() {
+        alert("Информация о местоположении недоступна");
+    }
+
+    if (!navigator.geolocation) {
+        error();
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
 }
